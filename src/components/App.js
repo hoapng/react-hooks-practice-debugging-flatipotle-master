ee235f93fea6e32f1067aebbb1059347054cfa75
hoapng <giahoa01giathieu@gmail.com>
2023-08-22 20:00:02 +0700

    debug

--- src/components/App.js
+++ src/components/App.js
@@ -11,8 +11,12 @@ function App() {
     setOrders(orders.concat(order));
   }
 
+  // const displayOrders = orders.map((order, idx) => {
+  //   <Order key={idx} {...order} />;
+  // });
+
   const displayOrders = orders.map((order, idx) => {
-    <Order key={idx} {...order} />;
+    return (<Order key={idx} {...order} />)
   });
 
   return (
@@ -20,9 +24,10 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
       </header>
-      <Form />
+      <Form addOrder={addOrder} />
       <div className="ui raised container segment">
         <h1 className="ui block header">All Orders</h1>
+        {console.log(displayOrders)}
         <div className="ui three cards">{displayOrders}</div>
       </div>
     </div>
